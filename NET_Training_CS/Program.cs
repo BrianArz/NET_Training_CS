9ebb88d01e2d7f58f965b2ae03a34f2b7bde1cc5
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-01 00:28:29 -0600

    Add project files.

---
+++ NET_Training_CS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NET_Training_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
