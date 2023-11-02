9a6fe66515b0891392522f5799b0683da59a89c7
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-02 08:17:29 -0600

    [Task1_2] + GetIsbn10()

--- Task1_2/Program.cs
+++ Task1_2/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            var output = CalculateMissingDivisibility(19, 11);
+            var output = GetIsbn10("819777048");
             Console.WriteLine($"Output: {output}");
+        }
+
+        static string GetIsbn10(string nineDigitString)
+        {
+            var digitSum = 0;
+            var stringCounter = 0;
+
+            for (int index = 10; index >= 2; index--)
+            {
+                //Numeric representation of a char with - '0'
+                var digit = nineDigitString[stringCounter] - '0';
+                digitSum += index * digit;
+                stringCounter++;
+            }
+
+            var checkDigit = CalculateMissingDivisibility(digitSum, 11).ToString();
+            if (checkDigit == "10") checkDigit = "X";
+
+            return nineDigitString + checkDigit;
         }
 
         static int CalculateMissingDivisibility(int numberA, int numberB)
