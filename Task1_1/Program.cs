9bbefca974c6a8216ca19f8815b1285059556bc9
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-02 07:35:11 -0600

    [Task!1_1] + User input + comments

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -6,14 +6,34 @@ namespace Task1_1
     {
         static void Main(string[] args)
         {
-            var output = FindDoublesAsNumbers(1, 1700);
-            Console.WriteLine("Output: ");
+            /*
+             * Assuming the input has no mistakes, that means:
+             * 1. User input two numbers
+             * 2. Number b is greater that number a
+             * 3. User input positive numbers
+             */
+
+            Console.WriteLine("Input first number: (a)");
+            var inputA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInput second number (b)");
+            var inputB = int.Parse(Console.ReadLine());
+
+            var output = FindDoublesAsNumbers(inputA, inputB);
+            Console.WriteLine("\nOutput: ");
             foreach (var number in output)
             {
                 Console.WriteLine(number);
             }
         }
 
+
+        /// <summary>
+        /// Returns a int list with all the numbers between a range that contains exactly two 'A' on their duodecimal representation
+        /// </summary>
+        /// <param name="start">Begining of the range</param>
+        /// <param name="end">Ending of the range</param>
+        /// <returns></returns>
         static List<int> FindDoublesAsNumbers(int start, int end)
         {
             var resultNumbers = new List<int>();
@@ -30,6 +50,11 @@ namespace Task1_1
             return resultNumbers;
         }
 
+        /// <summary>
+        /// Return duodecimal representation (string) of a decimal number
+        /// </summary>
+        /// <param name="decimalNumber">Decimal number to convert</param>
+        /// <returns></returns>
         static string DecimalBaseToDuodecimalBase(int decimalNumber)
         {
             const string duodecimalSymbols = "0123456789AB";
