20575f80226e0f3f4c594036d0e289a49ce76455
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-02 08:01:54 -0600

    [Task1_2] + CalulateMissingDivisibility()

---
+++ Task1_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = CalculateMissingDivisibility(19, 11);
+            Console.WriteLine($"Output: {output}");
+        }
+
+        static int CalculateMissingDivisibility(int numberA, int numberB)
+        {
+            var result = numberA % numberB;
+            return result != 0 ? numberB - result : 0;
+        }
+    }
+}
