5abb49209426de04ada655f20090399ae91d3fee
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-02 07:15:14 -0600

    [Task1_1] + DecimalBaseToDuodecimalBase()

---
+++ Task1_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = DecimalBaseToDuodecimalBase(1999);
+            Console.WriteLine($"Output: {output}");
+        }
+
+        static string DecimalBaseToDuodecimalBase(int decimalNumber)
+        {
+            const string duodecimalSymbols = "0123456789AB";
+
+            if (decimalNumber == 0) 
+                return "0";
+
+            var duodecimalNumber = "";
+
+            while (decimalNumber > 0)
+            {
+                duodecimalNumber = duodecimalSymbols[decimalNumber % 12] + duodecimalNumber;
+                decimalNumber /= 12;
+            }
+
+            return duodecimalNumber;
+        }
+    }
+}
