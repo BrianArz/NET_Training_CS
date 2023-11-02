651b70bd35deb2066816e81db320d0e59352348b
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-02 07:28:14 -0600

    [Task1_1] + FindDoubleAsNumbers()

--- Task1_1/Program.cs
+++ Task1_1/Program.cs
@@ -1,11 +1,33 @@
-﻿namespace Task1_1
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Task1_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var output = DecimalBaseToDuodecimalBase(1999);
-            Console.WriteLine($"Output: {output}");
+            var output = FindDoublesAsNumbers(1, 1700);
+            Console.WriteLine("Output: ");
+            foreach (var number in output)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static List<int> FindDoublesAsNumbers(int start, int end)
+        {
+            var resultNumbers = new List<int>();
+            for (var index = start; index <= end; index++)
+            {
+                var duodecimalNumber = DecimalBaseToDuodecimalBase(index);
+                var counterA = 0;
+                foreach (var character in duodecimalNumber)
+                {
+                    if(character == 'A') counterA++;
+                }
+                if(counterA == 2) resultNumbers.Add(index);
+            }
+            return resultNumbers;
         }
 
         static string DecimalBaseToDuodecimalBase(int decimalNumber)
