d2fd288eddb6dfef07a9aa69c1dabbe03fe4800b
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-08 01:30:11 -0600

    [Task2_2] + DiagonalMatrix class

---
+++ Task2_2/DiagonalMatrix.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Task2_2
+{
+    internal class DiagonalMatrix
+    {
+        private readonly int[] _mainDiagonal;
+        public int Size => _mainDiagonal.Length;
+
+        public DiagonalMatrix(params int[] elements)
+        {
+            _mainDiagonal = elements ?? Array.Empty<int>(); 
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i != j)
+                    return 0;
+                if (i < 0 || i >= Size)
+                    return 0;
+                return _mainDiagonal[i];
+            }
+            set
+            {
+                if (i == j && (j >= 0 && j < Size))
+                    _mainDiagonal[i] = value;
+            }
+        }
+
+    }
+}
---
+++ Task2_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Task2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test constructor
+            var diagonalOne = new DiagonalMatrix(1, 2, 3, 4, 5);
+            Console.WriteLine("Original Main Diagional:");
+            PrintMainDiagional(diagonalOne);
+
+            //Read checks
+            Console.WriteLine($"\nCheck read index[3,3] = {diagonalOne[3, 3]}");
+            Console.WriteLine($"Check read index[3,2] = {diagonalOne[3, 2]}");
+            Console.WriteLine($"Check read index[7,7] = {diagonalOne[7, 7]}");
+            Console.WriteLine($"Check read index[-2,-2] = {diagonalOne[3, 2]}");
+
+            //Modified diagonal one
+            diagonalOne[3, 3] = 7;
+
+            //This wont do anything
+            diagonalOne[3, 2] = 6;
+            diagonalOne[7, 7] = 9;
+            diagonalOne[-2, -2] = 10;
+
+            //Write checks
+            Console.WriteLine($"\nModified Main Diagional:");
+            PrintMainDiagional(diagonalOne);
+        }
+
+        static void PrintMainDiagional(DiagonalMatrix diagonal)
+        {
+            for (int index = 0; index < diagonal.Size; index++)
+            {
+                Console.WriteLine($"MainDiagional[{index}][{index}] = {diagonal[index, index]}");
+            }
+        }
+    }
+}
