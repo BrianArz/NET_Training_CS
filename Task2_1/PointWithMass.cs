699110f7491ee236fd0a78851b3506c0be7fc5ec
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-08 00:31:58 -0600

    [Task2_1] + PointWithMass class

---
+++ Task2_1/PointWithMass.cs
@@ -0,0 +1,41 @@
+﻿namespace Task2_1
+{
+    internal class PointWithMass
+    {
+        private int[] _coordinates = new int[3];
+        private double _mass;
+
+        public PointWithMass(int x, int y, int z, double mass)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            Mass = mass >= 0 ? mass : 0;
+        }
+
+        public int X
+        {
+            get => _coordinates[0];
+            set => _coordinates[0] = value;
+        }
+
+        public int Y
+        {
+            get => _coordinates[1];
+            set => _coordinates[1] = value;
+        }
+
+        public int Z
+        {
+            get => _coordinates[2];
+            set => _coordinates[2] = value;
+        }
+
+        //Mass cannot be less than zero
+        public double Mass
+        {
+            get => _mass;
+            set => _mass = value >= 0 ? value : 0;
+        }
+    }
+}
---
+++ Task2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test Constructor
+            var pointOne = new PointWithMass(1,2,3,4);
+
+            pointOne.Mass = -2;
+            pointOne.X = 3;
+            pointOne.Y = 4;
+            pointOne.Z = 5;
+
+            Console.WriteLine($"Point One: X = {pointOne.X} ; Y = {pointOne.Y} ; Z = {pointOne.Z} ; Mass = {pointOne.Mass}");
+
+        }
+    }
+}
