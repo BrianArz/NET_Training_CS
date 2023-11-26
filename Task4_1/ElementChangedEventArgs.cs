275d924106cf0a40db6d2f3cc859831b01ff17b4
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-26 11:34:51 -0600

    [Task4_1] Finish GenericDiagonalMatrix implementations

---
+++ Task4_1/ElementChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4_1
+{
+    internal class ElementChangedEventArgs<T> : EventArgs
+    {
+        public int Row { get; }
+        public int Column { get; }
+        public T OldValue { get; }
+        public T NewValue { get; }
+
+        public ElementChangedEventArgs(int index, T oldValue, T newValue)
+        {
+            Row = Column = index;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
---
+++ Task4_1/GenericDiagonalMatrix.cs
@@ -0,0 +1,66 @@
+﻿namespace Task4_1
+{
+    internal class GenericDiagonalMatrix<T>
+    {
+        private readonly T[] _mainDiagonal;
+        public int Size { get; }
+
+        public event EventHandler<ElementChangedEventArgs<T>> ElementChanged;
+
+        public GenericDiagonalMatrix(int size)
+        {
+            if(size < 0)
+                throw new ArgumentException("Matrix size can not be less than 1");
+
+            Size = size;
+
+            _mainDiagonal = new T[Size];
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                if (i != j)
+                    return default(T);
+
+                if (i < 0 || i >= Size)
+                    throw new IndexOutOfRangeException();
+
+                return _mainDiagonal[i];
+            }
+            set
+            {
+                if (i == j && (j >= 0 && j < Size))
+                {
+                    if (!Equals(_mainDiagonal[i], value))
+                    {
+                        OnElementChanged(new ElementChangedEventArgs<T>(i,  _mainDiagonal[i], value));
+                    }
+                }
+                _mainDiagonal[i] = value;
+            }
+        }
+
+        protected virtual void OnElementChanged(ElementChangedEventArgs<T> e)
+        {
+            ElementChanged?.Invoke(this, e);
+        }
+
+        public override string ToString()
+        {
+            var output = "";
+            for (var rowIndex = 0; rowIndex < Size; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < Size; columnIndex++)
+                {
+                    output += rowIndex == columnIndex ? _mainDiagonal[rowIndex] : 0;
+                    output += "\t";
+                }
+
+                output += "\n";
+            }
+            return output;
+        }
+    }
+}
