ab478ce925364042c1d43f8ba75360ec0ddad824
Brian Arizmendi <barizmendia1500@alumno.ipn.mx>
2023-11-26 14:35:26 -0600

    [Task5_2] Finish Book and Catalog implementation

---
+++ Task5_2/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Task5_2
+{
+    internal class Book
+    {
+        public string Title { get; }
+        public DateOnly? PublicationDate { get; }
+        public List<string>? Authors { get; }
+        
+        public Book(string title, DateOnly? publicationDate, List<string>? authors)
+        {
+            if (string.IsNullOrEmpty(title)) throw new ArgumentException("Title can not be null");
+
+            Title = title;
+            PublicationDate = publicationDate;
+            Authors = authors != null ? authors.Distinct().ToList() : new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string authorsString = string.Join(", ", Authors);
+            return $"Title: {Title}, Publication Date: {PublicationDate}, Authors: {authorsString}";
+        }
+    }
+}
---
+++ Task5_2/Catalog.cs
@@ -0,0 +1,40 @@
+﻿namespace Task5_2
+{
+    internal class Catalog
+    {
+        private Dictionary<string, Book> _catalog = new Dictionary<string, Book>();
+
+        private string UnformatIsbn(string isbn)
+        {
+            return isbn.Replace("-", "");
+        }
+
+        public void AddBook(Book book, string isbn)
+        {
+            _catalog[UnformatIsbn(isbn)] = book;
+        }
+
+        public Book GetBook(string isbn)
+        {
+            if (_catalog.TryGetValue(UnformatIsbn(isbn), out var book))
+                return book;
+            else
+                throw new Exception("Book not found");
+        }
+
+        public IEnumerable<(string Isbn, string Title)> GetTitlesAlphabetically()
+        {
+            return _catalog.OrderBy(b => b.Value.Title).Select(pair => (Isbn: pair.Key, pair.Value.Title));
+        }
+
+        public IEnumerable<(string Isbn, Book book)> GetBooksByAuthor(string author)
+        {
+            return _catalog.Where(b => b.Value.Authors.Contains(author)).OrderBy(b => b.Value.PublicationDate).Select(pair => (Isbn: pair.Key, pair.Value));
+        }
+
+        public IEnumerable<(string Author, int BookCount)> GetAuthorsBookCount()
+        {
+            return _catalog.Values.SelectMany(b => b.Authors).GroupBy(author => author).Select(pair => (Author: pair.Key, BookCount: pair.Count()));
+        }
+    }
+}
